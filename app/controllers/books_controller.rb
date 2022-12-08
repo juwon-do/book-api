923468562a9da4fa9ed60653d604f97295c7b473
Juwon Do <nobeldr13@gmail.com>
2022-12-08 00:15:44 -0600

    add index, create, show, update, destroy of book api app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,35 @@
 class BooksController < ApplicationController
+  def index
+    books = Book.all
+    render json: books.as_json
+  end 
+  def show
+    book = Book.find_by(id: params[:id])
+    render json: book.as_json
+  end
+  def create
+    book = Book.new(
+      id: params[:input_id],
+      name: params[:input_name],
+      author: params[:input_author],
+      price: params[:input_price],
+      isbn: params[:input_isbn]
+    )
+    book.save
+    render json: book.as_json
+  end
+  def update
+    book = Book.find_by(id: params[:id])
+    book.name = params[:name]
+    book.author = params[:author]
+    book.price = params[:price]
+    book.isbn = params[:isbn]
+    book.save
+    render json: book.as_json
+  end
+  def destroy
+    book = Book.find_by(id: params[:id])
+    book.destroy
+    render json: book.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/books" => "books#index"
+  get "/books/:id" =>"books#show"
+  post "/books" => "books#create"
+  patch "/books/:id" => "books#update"
+  delete "/books/:id" => "books#destroy"
 end
